fdb0416ae67813838c947ebbfaa2f7151663ebe1
Merline Belizaire <merline@noukod.com>
2019-05-03 19:16:11 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import Actors from '../components/Actors';
+import Directors from '../components/Directors';
+import Movies from '../components/Movies';
 
-const NavBar = () => {
+
+const App = (props) => {
   return (
-    <div className="navbar">
-      <NavLink to="/">Home</NavLink><br/>
-      <NavLink to="/movies">Movies</NavLink><br/>
-      <NavLink to="/directors">Directors</NavLink><br/>
-      <NavLink to="/actors">Actors</NavLink>
-    </div>
+    <Router>
+      <React.Fragment>
+      <div className="navbar">
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/directors" component={Directors} />
+          <Route exact path="/actors" component={Actors} />
+        </div>
+      </React.Fragment>   
+    </Router>
   );
 };
 
-export default NavBar;
+export default App
